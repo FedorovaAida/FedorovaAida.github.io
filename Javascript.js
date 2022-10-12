52f15634d42f79ded4ab92ce12571182de1de15d
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2022-10-12 20:16:19 +0300

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+function onClick()
+{
+  var quantity=document.getElementById("quantity").value;
+  var price=document.getElementById("price").value;
+  var sum_money=document.getElementById("sum_money");
+  var kol; var sum;
+  sum_money.innerHTML = "";
+  kol = quantity; sum = price;
+  
+  if (kol>0 && kol!=NaN && sum>0 && sum!=NaN)
+    sum_money.innerHTML=kol*sum;    
+  else sum_money.innerText="Неккоретно заполнены значения, попробуйте еще раз";    
+}
+window.document.addEventListener("DOMContentLoaded", 
+function (event) 
+{
+  console.log("DOM fully loaded and parsed");
+  var sum = document.getElementById("btn");
+  sum.addEventListener("click", onClick);
+});
