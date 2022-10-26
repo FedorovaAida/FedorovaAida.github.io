dce1e9141335f3fb32f8e2e7184c128700faf4ef
FedorovaAida <112868534+FedorovaAida@users.noreply.github.com>
2022-10-26 09:11:05 +0300

    Update JavaScript7.js

--- JavaScript7.js
+++ JavaScript7.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $(".picture").slick({
+  $(".slider").slick({
     slidesToShow:4,
      slidesToScrol:1,
     dots: true,
     responsive:[
       {
-        breakpoint: 1024,
+        breakpoint: 560,
         settings:{
           slidesToShow:3,
      slidesToScrol:1,
